67f2122da782f8053dddb2c15bb74428a5233f90
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-07-14 14:07:17 +0600

    using hash map

--- Hashmap.dart
+++ Hashmap.dart
@@ -9,7 +9,7 @@ void main(List<String> args) {
 
 gethashs(){
   var val = new HashMap();
-
+  
 
     var data = {
       "name":"sajib",
@@ -18,13 +18,31 @@ gethashs(){
       "name3":"sajib",
     };
 
+    var arr = [ "sajib","12",234,234,234,234,234,234,234,"dart","javascript",null,""];
+    var news = [];
+
   val.addAll(data);
 
-  val.forEach((key, value) =>{
-    print(key)
-  },);
+// print(arr);
 
-    print(val);
+  arr.removeWhere((value) => value == null || value == "" || value is int); //remove 
+  arr.removeWhere((value) => value == null || value == "" || value is String); //remove 
+
+  // for(var i =0 ; i<arr.length;i++){
+
+  
+  //   if( arr[i] is String){
+  //         // news.add(arr[i]);
+  //         arr.add(arr[i]);
+  //           // arr.remove(arr[i]);
+  //           // print(arr[i]);
+  //           // news.remove(arr[i]);
+  //       }
+  // }
+
+   
+  
+ print(arr) ;
 
 }
 
