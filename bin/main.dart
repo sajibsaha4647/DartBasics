4940feaccbfed2472dee02487330d1b54384b731
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-07-14 10:49:57 +0600

    updated

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,34 @@
 
+
 void  main() {
 sajib();
 }
 
  var lst = []; 
+ var lst2 = [];
 sajib(){
   
   var data =  {"name": "Tom", 'Email': 'tom@xyz.com'};
-  
+
   var adds = {
     "name": "Tom", 'Email': 'tom@xyz.com'
   } ;
 
-    lst.add(adds);
+    // lst.add(adds);
 
   var res ;
-  for(var i= 0 ; i < lst.length; i++){
-      res = lst[i];
-      print(res);
-      res.forEach((k,v) => {
-        print('${k}: ${v}')
-      }); 
+  // for(var i= 0 ; i < lst.length; i++){ //make loop in a list
+  //     res = lst[i];
+  //     // print(res);
+     
        
-  }
+  // }
+
+   data.forEach((k,v) => { //make loop in object
+        // print('${k}: ${v}')
+        lst2.add(k.toString())
+      }); 
+
+  print(lst2+lst); //marge array
 
 }
