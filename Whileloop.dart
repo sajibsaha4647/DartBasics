385ca7d043d651b8f9baf93f91206b357d61cd63
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-07-14 11:15:45 +0600

    updated

---
+++ Whileloop.dart
@@ -0,0 +1,15 @@
+ void main(List<String> args) {
+  
+}
+
+getwhileLoop(){
+
+  var data = 0;
+
+
+  while(data < 10){
+    data++;
+    print(data);
+  }
+
+}
---
+++ doWhile.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  getdowhileloop();
+}
+
+getdowhileloop(){
+
+
+var i = 0;
+
+do{
+
+print(i);
+i++;
+
+}while(i<10);
+
+}
